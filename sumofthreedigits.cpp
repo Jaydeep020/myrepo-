4b8a1fc9e258dc7541977c0feeb2147f68486010
Jaydeep020 <goodsuccess8871@gmail.com>
2023-01-10 00:10:32 +0530

    second commit

---
+++ sumofthreedigits.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int a,first,second,third; 
+ int sum =0;
+cout<<"enter the three digit number: ";
+cin>>a;
+first = a/100;
+a=a%100;
+second = a/10;
+a = a%10;
+third = a;
+cout<<"the sum of digits is : "<<first+second+third;
+return 0;
+}
